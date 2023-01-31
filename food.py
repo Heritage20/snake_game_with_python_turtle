b320e812c43014db80ea1184a144f2e6bbe0ac3a
Abraham Fatoki <60294083+Heritage20@users.noreply.github.com>
2023-01-31 13:19:35 +0100

    managed the updated version

--- food.py
+++ food.py
@@ -3,10 +3,8 @@ import random
 
 
 class Food(Turtle):
-    """defines the food by inheriting from the Turtle class"""
 
     def __init__(self):
-        """defines the Food class which inherits from the Turtle class to create a food for the snake"""
         super().__init__()
         self.shape("circle")
         self.penup()
@@ -16,7 +14,6 @@ class Food(Turtle):
         self.refresh()
 
     def refresh(self):
-        """update the food to a random location once eaten by the snake"""
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Screen
 from snake import Snake
 from food import Food
@@ -21,29 +22,27 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 game_is_on = True
-
 while game_is_on:
-    # removes the distance in each snake movement
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    # detect collision with food
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    # detect collision with wall
+    # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
-    # detect collision with tail
+    # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
